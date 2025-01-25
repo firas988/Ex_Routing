c8edc8f9cede862a86891c9762fc4cd69eae52bf
firas najjar <162709181+firas988@users.noreply.github.com>
2025-01-25 13:00:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,35 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-
-const dirPath = path.join(__dirname, "/templates");
-
-//read html file
-let file2send = fs.readFileSync(`${dirPath}/page.html`);
+const dirPath = path.join(__dirname, "templates");
 
 const server = http.createServer((req, res) => {
   const urlObj = url.parse(req.url);
   const pathname = urlObj.pathname;
 
-  if (pathname.match("[.]html$")) {
+  //if pathname / we load the home page
+  if (pathname === "/") {
     res.writeHead(200, "OK", { "content-type": "text/html" });
-    const reader = fs.createReadStream(
-      path.join(__dirname, "templates", pathname)
-    );
+    const reader = fs.createReadStream(path.join(dirPath, "page.html"));
     reader.pipe(res);
-  } else if (pathname.match("[.]css$")) {
+  }
+
+  //if request path was ending with an html we load it
+  else if (pathname.match("[.]html$")) {
+    res.writeHead(200, "OK", { "content-type": "text/html" });
+    const reader = fs.createReadStream(path.join(dirPath, pathname));
+    reader.pipe(res);
+  }
+
+  //if it ends with css with load it
+  else if (pathname.match("[.]css$")) {
     res.writeHead(200, "OK", { "content-type": "text/css" });
-    const reader = fs.createReadStream(
-      path.join(__dirname, "templates", pathname)
-    );
+    const reader = fs.createReadStream(path.join(dirPath, pathname));
     reader.pipe(res);
-  } else {
+  }
+
+  //if it doesnt match any of what we're able to load then it'll send a not found page
+  else {
     res.writeHead(404, "Not found", { "content-type": "text/html" });
     const error = "<h1>Page Not Found</h1>";
     res.end(error);
